f146727cd3b1d94773bd76adcf554cb31669f4a9
Yevgen Kravchenko <yevgen.kravchenko@amadeus.com>
2017-08-17 11:45:34 +0300

    README content amended

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Example of REST API test automation using Rest Assured + Java + Maven
+
+# To run Tests from Jenkins or console use command:
+# mvn -Dendpoint="${endpoint} -Dtest.xml=${test.xml} -Dapp_id="${app_id} -Dapp_code="${app_code}" clean test
+
+# e.g. mvn -Dendpoint=https://weather.cit.api.here.com/weather/1.0/report.json -Dtest.xml=here_weather_tests.xml -Dapp_id=DemoAppId01082013GAL -Dapp_code=AJKnXv84fjrb0KIHawS0Tg clean test
--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
     public Map<String, String> headers;
 
     /**
-     * To run Tests from Jenkins or console using defined test.xml and endpoint
+     * To run Tests from Jenkins or console use command:
      * mvn -Dendpoint="${endpoint} -Dtest.xml=${test.xml} -Dapp_id="${app_id} -Dapp_code="${app_code}" clean test
      *
      * e.g. mvn -Dendpoint=https://weather.cit.api.here.com/weather/1.0/report.json -Dtest.xml=here_weather_tests.xml -Dapp_id=DemoAppId01082013GAL -Dapp_code=AJKnXv84fjrb0KIHawS0Tg clean test
